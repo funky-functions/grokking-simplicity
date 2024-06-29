728980f844357b5ec7454e6cc582e763ba5c152b
jong-k <fully.charged07@gmail.com>
2024-06-29 13:09:04 +0900

    feat: ch5 내용 정리

---
+++ notes/kjh/ch5/megaMart.ts
@@ -0,0 +1,97 @@
+interface ShoppingItem {
+  name: string;
+  price: number;
+}
+
+let shoppingCart: ShoppingItem[] = []; // A
+let shoppingCartTotal = 0; // A
+
+function addItemToCart(name: string, price: number) {
+  // A
+  shoppingCart = addItem(shoppingCart, name, price);
+  calcCartTotal();
+}
+
+function addItem(cart: ShoppingItem[], name: string, price: number) {
+  // C
+  return [...cart, { name, price }];
+}
+
+function calcCartTotal() {
+  // A
+  shoppingCartTotal = calcTotal(shoppingCart);
+  // set_cart_total_dom(); // 금액 합계를 DOM에 반영
+  // 전달받은 장바구니 사용
+  // update_shipping_icons(shoppingCart); // 구매 아이콘 업데이트
+  // update_tax_dom(); // 세금을 DOM에 반영
+}
+
+function updateShippingIcons() {
+  // A
+  // const buy_buttons = get_buy_buttons_dom();
+  // buy_buttons.forEach((button) => {
+  //   const item = button.item;
+  // 리팩토링1. 적용
+  //   const newCart = add_item(shoppingCart, item.name, item.price);
+  //   if (refactoredIsFreeShipping(newCart)) {
+  //      button.show_free_shipping_icon();
+  //   } else {
+  //      button.hide_free_shipping_icon();
+  //   }
+  // });
+}
+
+function updateTaxDom() {
+  // A
+  // setTaxDom(calcTax(shoppingCartTotal);
+}
+
+function calcTotal(shoppingCart: ShoppingItem[]) {
+  // C
+  return shoppingCart.reduce((acc, cur) => acc + cur.price, 0);
+}
+
+function isFreeShipping(price: number, totalPrice: number) {
+  // C
+  return price + totalPrice >= 20;
+}
+
+function calcTax(amount: number) {
+  // C
+  return amount * 0.1;
+}
+
+// -------- 리팩토링1. 중복 줄이기 --------
+// TODO: 1. 현재 장바구니 합계만 가지고 무료 배송 여부를 판단
+// TODO: 2. 코드 중복 줄이기
+
+function refactoredIsFreeShipping(cart: ShoppingItem[]) {
+  // calcTotal 계산 재사용
+  return calcTotal(cart) >= 20;
+}
+
+// -------- 리팩토링2. 암묵적 입출력 줄이기 --------
+// TODO: 1. 전역변수 읽는 코드 바꾸기
+
+function refactoredUpdateShippingIcons(cart: ShoppingItem[]) {
+  // const buy_buttons = get_buy_buttons_dom();
+  // buy_buttons.forEach((button) => {
+  //   const item = button.item;
+  // 전역 변수 대신 매개변수로 받은 장바구니 전달
+  //   const newCart = add_item(cart, item.name, item.price);
+  //   if (refactoredIsFreeShipping(newCart)) {
+  //      button.show_free_shipping_icon();
+  //   } else {
+  //      button.hide_free_shipping_icon();
+  //   }
+  // });
+}
+
+// -------- 리팩토링3. 유틸 함수 활용 --------
+// TODO: 1. 장바구니 뿐만 아니라 배열에 원소 추가할 때 어디서나 재사용 가능
+function add_element_last(array: any[], elem: any) {
+  // copy-on-write 패턴: 값을 바꿀 때 복사
+  const new_array = array.slice();
+  new_array.push(elem);
+  return new_array;
+}
---
+++ notes/kjh/ch5/quiz.ts
@@ -0,0 +1,74 @@
+export {};
+// 연습문제 p.93
+// 전역 변수 읽는 부분을 찾아 바꾸기
+// 1. 전역 변수 shopping_cart 읽는 부분 1군데만 남기고 모두 제거
+// 2. 미사용 shopping_cart_total 제거
+interface ShoppingItem {
+  name: string;
+  price: number;
+}
+
+let shopping_cart: ShoppingItem[] = [];
+
+function add_item(cart: ShoppingItem[], name: string, price: number) {
+  return [...cart, { name, price }];
+}
+
+function add_item_to_cart(name: string, price: number) {
+  // 유일하게 전역 변수 읽는 부분
+  shopping_cart = add_item(shopping_cart, name, price);
+  const total = calc_total(shopping_cart);
+  set_cart_total_dom(total);
+  update_shipping_icons(shopping_cart);
+  update_tax_dom(total);
+  shopping_cart_total = total;
+}
+
+function calc_total(shoppingCart: ShoppingItem[]) {
+  return shoppingCart.reduce((acc, cur) => acc + cur.price, 0);
+}
+
+// 전역 변수 참조 대신 매개변수 추가
+// 내부에서 호출하는 함수들에도 인수 전달
+// 함수만의 역할 존재감이 없어서 호출하는 곳으로 합침
+// function calc_cart_total(cart: ShoppingItem[]) {
+//   const total = calc_total(cart);
+//   set_cart_total_dom(total);
+//   update_shipping_icons(cart);
+//   update_tax_dom(total);
+//   shopping_cart_total = total;
+// }
+
+function set_cart_total_dom(total: number) {
+  //...
+}
+
+function update_shipping_icons(cart: ShoppingItem[]) {
+  // const buttons = get_buy_buttons_dom();
+  // buttons.forEach((button) => {
+  //   const item = button.item;
+  //   const new_cart = add_item(cart, item.name, item.price);
+  //   if (gets_free_shipping(new_cart)) button.show_free_shipping_icon();
+  //   else button.hide_free_shipping_icon();
+  // });
+}
+
+function get_buy_buttons_dom() {
+  // ...
+}
+
+function gets_free_shipping() {
+  // ...
+}
+
+function update_tax_dom(total: number) {
+  set_tax_dom(calc_tax(total));
+}
+
+function calc_tax(amount: number) {
+  return amount * 0.1;
+}
+
+function set_tax_dom(tax: number) {
+  // ...
+}
